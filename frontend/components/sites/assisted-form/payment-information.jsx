76216c83476ce68b1ac5429b16cf6c6e8bf1101a
Abhilash Mandula <mandula.abhilash@gmail.com>
2025-03-01 16:33:03 +0530

    Update Assisted Submission Form

--- frontend/components/sites/assisted-form/payment-information.jsx
+++ frontend/components/sites/assisted-form/payment-information.jsx
@@ -22,9 +22,6 @@ export function PaymentInformation({
 }) {
   // Calculate payment details
   const basePrice = 250;
-  const bidManagementFee = manageBidsProcess ? 2750 : 0;
-  const totalPrice = basePrice + bidManagementFee;
-
   // Use state to control height
   const [contentHeight, setContentHeight] = React.useState(
     manageBidsProcess ? "auto" : "auto"
@@ -67,11 +64,12 @@ export function PaymentInformation({
               <div className="flex items-center">
                 <div>
                   <h3 className="font-semibold">
-                    FG+B to manage the bids process
+                    I'm interested in FG+B managing the bids process
                   </h3>
                   <p className="text-sm text-muted-foreground">
-                    Includes chasing, handling queries, and providing a tender
-                    summary report
+                    This option will incur an additional fee which will be
+                    payable by the buyer. For more information on this fee
+                    please contact andy@fgandb.co.uk
                   </p>
                 </div>
               </div>
@@ -86,26 +84,12 @@ export function PaymentInformation({
             </div>
           </div>
 
-          {/* Container with absolute height */}
-          <div className="h-[50px]">
-            {manageBidsProcess && (
-              <div className="flex justify-between items-center pt-2 pl-6">
-                <div>
-                  <p className="text-sm font-semibold">Bids Management Fee</p>
-                </div>
-                <div className="text-lg font-medium">
-                  £{bidManagementFee.toFixed(2)}
-                </div>
-              </div>
-            )}
-          </div>
-
           <div className="flex justify-between items-center pt-3 border-t-2">
             <div>
               <h3 className="font-bold text-lg">Total</h3>
             </div>
             <div className="text-xl font-bold text-web-orange">
-              £{totalPrice.toFixed(2)}
+              £{basePrice.toFixed(2)}
             </div>
           </div>
 
