56fd71eb34ff80dc1d0a899bf790e87a2b21ba5b
Abhilash Mandula <mandula.abhilash@gmail.com>
2025-03-24 17:12:30 +0530

    Remove unused imports from desktop nav

--- frontend/components/dashboard-nav/desktop-nav.jsx
+++ frontend/components/dashboard-nav/desktop-nav.jsx
@@ -1,25 +1,17 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
 import { useAuth } from "@/visdak-auth/src/hooks/useAuth";
 
 import { BuyerSidebar } from "@/components/filters/sidebar/buyer-sidebar";
 import { SellerSidebar } from "@/components/filters/sidebar/seller-sidebar";
 
 export function DesktopNav({
-  activeTab,
   filters,
   onFilterChange,
   viewMode,
   onViewModeChange,
 }) {
   const { user } = useAuth();
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const handleSubmitNewSite = () => {
-    router.push("/dashboard/sites/options");
-  };
 
   if (user?.role === "buyer") {
     return (
